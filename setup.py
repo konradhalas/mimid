98205c7e83efff4d866e069bbfdbe1fb1579d1ac
konradhalas <halas.konrad@gmail.com>
2020-12-01 16:59:15 +0100

    Add Python 3.9 support

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Testing",
         "Topic :: Software Development :: Testing :: Mocking",
