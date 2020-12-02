fca4b61c5ee9e5bbb2e60f9a3fcc3593a30333d2
konradhalas <halas.konrad@gmail.com>
2020-12-02 16:56:11 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mimid",
-    version="0.0.7",
+    version="0.0.8",
     description="Modern mocking library for Python.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
